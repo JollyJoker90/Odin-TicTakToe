f89d77f7dbc7803f1875c0049510ca6327823b4f
JollyJoker90 <eikeheidel@googlemail.com>
2024-12-12 09:48:36 +0100

    added more user interface ability to start and restart game ability to change user names

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
 const GameBoard = () => {
   const rows = 3;
   const columns = 3;
-  board = [];
-  for (let i = 0; i < rows; i++) {
-    board[i] = [];
-    for (let j = 0; j < columns; j++) {
-      board[i].push(".");
+  let board = [];
+  const initializeBoard = () => {
+    board = [];
+    for (let i = 0; i < rows; i++) {
+      board[i] = [];
+      for (let j = 0; j < columns; j++) {
+        board[i].push(".");
+      }
     }
-  }
+  };
 
   const getBoard = () => {
     return board;
   };
 
-  const displayBoard = () => {
-    console.log(board);
-  };
+  // const displayBoard = () => {
+  //   console.log(board);
+  // };
 
   const placeMarker = (row, col, player) => {
     if (board[row][col] !== ".") return;
     board[row][col] = player.marker;
   };
 
-  return { getBoard, displayBoard, placeMarker };
+  initializeBoard();
+
+  return { getBoard, placeMarker, initializeBoard };
 };
 
 function Player(name, marker) {
-  return { name, marker };
+  const setName = (n) => {
+    name = n;
+  };
+  return { name, marker, setName };
 }
 
 function GameController() {
   const board = GameBoard();
   const players = [Player("Player1", "x"), Player("Player2", "o")];
+  let winner;
 
   let activePlayer = players[0];
 
@@ -39,16 +48,15 @@ function GameController() {
     return activePlayer;
   };
 
-  const printNewRound = () => {
-    board.displayBoard();
-  };
+  // const printNewRound = () => {
+  //   board.displayBoard();
+  // };
 
   const switchPlayer = () => {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
   };
 
   const checkWinConditions = () => {
-    // console.log(board.getBoard());
     let currentBoard = board.getBoard();
     let winnerFound =
       currentBoard.some((row) =>
@@ -64,48 +72,79 @@ function GameController() {
         currentBoard[1][1] === activePlayer.marker &&
         currentBoard[0][2] === activePlayer.marker);
     if (winnerFound) {
-      console.log(`${activePlayer.name} won!`);
+      // console.log(`${activePlayer.name} won!`);
+      winner = activePlayer.name;
     } else if (currentBoard.every((row) => row.every((cell) => cell !== "."))) {
-      console.log("It's a tie!");
+      // console.log("It's a tie!");
+      winner = activePlayer.name;
     }
   };
 
   const playRound = (row, col) => {
-    console.log(
-      `Placing ${activePlayer.marker} for ${activePlayer.name} in position ${row}:${col}..`
-    );
+    // console.log(
+    //   `Placing ${activePlayer.marker} for ${activePlayer.name} in position ${row}:${col}..`
+    // );
     board.placeMarker(row, col, activePlayer);
     checkWinConditions();
     switchPlayer();
-    // printNewRound();
   };
-  console.log(`Playing Tic Tak Toe\n${activePlayer.name} begins..`);
-  printNewRound();
 
-  return { playRound, getBoard: board.getBoard, getActivePlayer };
+  const restart = () => {
+    board.initializeBoard();
+  };
+
+  const getWinner = () => {
+    return winner;
+  };
+
+  const renamePlayer = (index, name) => {
+    players[index].name = name;
+  };
+
+  return {
+    playRound,
+    getBoard: board.getBoard,
+    getActivePlayer,
+    restart,
+    getWinner,
+    renamePlayer,
+  };
 }
 
 function ScreenController() {
   const game = GameController();
   const boardDiv = document.querySelector(".game-board");
+  const message = document.querySelector(".interface-message");
 
   const handleInterfaceInput = (e) => {
     if (boardDiv.classList.contains("hidden")) {
       boardDiv.classList.remove("hidden");
+      message.textContent = `${game.getActivePlayer().name}'s turn..`;
       e.target.textContent = "Restart";
     } else {
-      // restart game
+      boardDiv.innerHTML = "";
+      game.restart();
+      updateScreen();
     }
+  };
+
+  const handleUserNameChange = (e) => {
+    game.renamePlayer(e.target.dataset.index, e.target.value);
   };
 
   document
     .querySelector(".interface-button")
     .addEventListener("click", handleInterfaceInput);
 
+  document
+    .querySelectorAll(".player-names input")
+    .forEach((input) => input.addEventListener("change", handleUserNameChange));
+
   const updateScreen = () => {
-    // boardDiv.innerHTML = "";
     const board = game.getBoard();
 
+    if (!boardDiv.classList.contains("hidden"))
+      message.textContent = `${game.getActivePlayer().name}'s turn..`;
     board.forEach((row, indexRow) => {
       row.forEach((cell, indexCol) => {
         let cellButton = document.querySelector(
@@ -132,5 +171,4 @@ function ScreenController() {
   updateScreen();
 }
 
-// const game = GameController();
 ScreenController();
